72308cc4106f255ef8e1da29bf16cbe77d5e8028
josh <joshdotjs@gmail.com>
2023-03-17 01:04:22 +0000

    todo

--- src/App.js
+++ src/App.js
@@ -19,12 +19,34 @@ export default function App() {
       const users = await resp.json();
       console.log("users: ", users);
       setUsers(users);
+
+      {
+        /* 1. Add form for data to create user */
+      }
+      {
+        /* 2. POST request to endpoint to create new user */
+      }
+      {
+        /* 3. Endpoint to store new user in DB */
+      }
+      {
+        /* 4. Send response for success / error */
+      }
+      {
+        /* 5. Handle response with appropriate error-handling on frontend */
+      }
+      {
+        /* 6. Need notification system to display errors */
+      }
+      {
+        /* https://mui.com/material-ui/react-snackbar/ */
+      }
     })();
   }, []);
 
   // --------------------------------------------
 
-  const addUserHanlder = () => {
+  const addUserHandler = () => {
     alert("todo -");
   };
 
@@ -35,10 +57,10 @@ export default function App() {
       <h1>CRUD App</h1>
 
       <Stack spacing={2} direction="row">
-        <Button variant="outlined" onClick={addUserHanlder}>
-          Get User
+        <Button variant="outlined" onClick={addUserHandler}>
+          Delete User
         </Button>
-        <Button variant="contained" onClick={addUserHanlder}>
+        <Button variant="contained" onClick={addUserHandler}>
           Create User
         </Button>
       </Stack>
