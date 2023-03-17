ca37c50b3e1647f2a9f43ac007e431d1b6d44c2b
josh <joshdotjs@gmail.com>
2023-03-17 00:33:26 +0000

    updated privileges

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
   // --------------------------------------------
 
   const addUserHanlder = () => {
-    alert("todo");
+    alert("todo -");
   };
 
   // --------------------------------------------
